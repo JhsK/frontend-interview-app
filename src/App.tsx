5d947d12742b0f63fcc47135a718034e12526093
JhsK <qwe6293@nate.com>
2022-04-27 20:54:42 +0900

    tailwind setting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <h3 className="mt-5">ddd</h3>
+    </div>
+  );
+}
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  // eslint-disable-next-line prettier/prettier
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
